b1f5d2f72ac9682cef940e0560b4d27e9c7604c6
mampi <mampi.bhakta@envertis.com.au>
2020-01-14 17:54:00 +0530

    'sagnik'

---
+++ views/site/about.php
@@ -0,0 +1,20 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'About';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+	<center>
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        This is my study site for YII2, @sagnik_dey
+    </p>
+    </center>
+
+    <!-- <code><?= __FILE__ ?></code> -->
+</div>
